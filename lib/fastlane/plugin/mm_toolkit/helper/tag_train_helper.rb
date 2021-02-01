77de044f816e4db2bf09174729beda6a70b88922
Adrián García <adriangarcia.lopez@gmail.com>
2021-02-01 18:51:53 +0100

    Refactor tag_train

---
+++ lib/fastlane/plugin/mm_toolkit/helper/tag_train_helper.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+module Fastlane
+  module Actions
+    # Returns the latest tag from the current branch
+    def self.get_latest_tag_from_branch
+      sh("git describe --tags --abbrev=0").strip
+    rescue
+      nil
+    end
+
+    # Returns the latest version from the current branch
+    def self.get_latest_version_from_branch
+      tag = get_latest_tag_from_branch
+      version = tag.gsub(/v/, "")
+
+      version
+    end
+  end
+end
