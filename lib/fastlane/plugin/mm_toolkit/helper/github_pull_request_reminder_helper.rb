fd9b91275b4cdb5672760c1d4578c8c5b87a3c9d
Adrián García <adriangarcia.lopez@gmail.com>
2022-03-01 08:45:46 +0100

    Add Google Chat utility methods for Github mentions

--- lib/fastlane/plugin/mm_toolkit/helper/github_pull_request_reminder_helper.rb
+++ lib/fastlane/plugin/mm_toolkit/helper/github_pull_request_reminder_helper.rb
@@ -11,5 +11,10 @@ module Fastlane
     def self.get_github_user_login_mention_for_slack(user_github_login, slack_user_id)
       "#{user_github_login}:<@#{slack_user_id}>"
     end
+
+    # Returns the Google Chat handle formatted to be used with github_pull_request_reminder's user_logins_and_mentions param
+    def self.get_github_user_login_mention_for_google_chat(user_github_login, google_chat_user_id)
+      "#{user_github_login}:<users/#{google_chat_user_id}>"
+    end
   end
 end
