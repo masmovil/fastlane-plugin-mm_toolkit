17bb70be62dce448c81446b33f7a6e45f8a6b191
Raúl Pedraza <raul.pedraza@masmovil.com>
2023-05-12 15:51:43 +0200

    setup rows to Insert  in big quey

--- lib/fastlane/plugin/mm_toolkit/actions/insert_data_in_big_query.rb
+++ lib/fastlane/plugin/mm_toolkit/actions/insert_data_in_big_query.rb
@@ -10,21 +10,21 @@ module Fastlane
           project_id = params.fetch(:project_id)
           dataset_id = params.fetch(:dataset_id)
           table_id = params.fetch(:table_id)
-          #rows = params.fetch(:rows)
-          #rows = params[:rows]
+          rows = params.fetch(:rows)
 
-          setup(credentials, project_id, dataset_id, table_id)
+          setup(credentials, project_id, dataset_id, table_id, rows)
         end
   
         #####################################################
         # @!group support functions
         #####################################################
   
-        def self.setup(credentials, project_id, dataset_id, table_id)
+        def self.setup(credentials, project_id, dataset_id, table_id, rows)
           UI.important("Setup Big Query...")
           big_query_controller = BigQueryController.new(credentials, project_id, dataset_id, table_id)
-          #big_query_controller.insert(rows)
-          UI.success("Succesfully setup Big Query!")
+          UI.important("Insert rows in Big Query...")
+          big_query_controller.insert(rows)
+          UI.success("Succesfully insert rows in Big Query...!")
         end
   
         #####################################################
@@ -43,28 +43,34 @@ module Fastlane
           [
             FastlaneCore::ConfigItem.new(
               key: :credentials,
-              env_name: "FL_SETUP_BIG_QUERY_CREDENTIALS",
+              env_name: "FL_INSERT_ROWS_IN_BIG_QUERY_CREDENTIALS",
               description: "String or Hash with route or content at credentials file if you need more info go to https://www.rubydoc.info/gems/google-cloud-bigquery/Google/Cloud/Bigquery#new-class_method",              
               optional: true,
               default_value: ENV["GOOGLE_APPLICATION_CREDENTIALS"],
             ),
             FastlaneCore::ConfigItem.new(
               key: :project_id,
-              env_name: "FL_SETUP_BIG_QUERY_PROJECT_ID",
+              env_name: "FL_INSERT_ROWS_IN_BIG_QUERY_PROJECT_ID",
               description: "Big query project id",
               type: String,
               ),
             FastlaneCore::ConfigItem.new(
                 key: :dataset_id,
-                env_name: "FL_SETUP_BIG_QUERY_DATASET_ID",
+                env_name: "FL_INSERT_ROWS_IN_BIG_QUERY_DATASET_ID",
                 description: "Big query dataset id",
                 type: String,
               ),
               FastlaneCore::ConfigItem.new(
                 key: :table_id,
-                env_name: "FL_SETUP_BIG_QUERY_TABLE_ID",
+                env_name: "FL_INSERT_ROWS_IN_BIG_QUERY_TABLE_ID",
                 description: "Big query table id",
                 type: String,
+              ),
+              FastlaneCore::ConfigItem.new(
+                key: :rows,
+                env_name: "FL_INSERT_ROWS_IN_BIG_QUERY_TABLE_ID",
+                description: "Rows to insert in Big query",
+                type: Hash,
               ),
           ]
         end
