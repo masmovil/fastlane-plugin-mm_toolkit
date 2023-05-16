08098b6f0cca2caed44de2efbc79582107e03f62
Raúl Pedraza <raul.pedraza@masmovil.com>
2023-05-16 13:47:49 +0200

    finish insert rows action

--- lib/fastlane/plugin/mm_toolkit/helper/app_store_ax_connector/big_query_controller.rb
+++ lib/fastlane/plugin/mm_toolkit/helper/app_store_ax_connector/big_query_controller.rb
@@ -27,46 +27,19 @@ end
 
 def insert(data)
   if !@dataset.exists?
-    raise "Error dataset don't exist - #{result.error}"
+    raise "Error dataset doesn't exist - #{result.error}"
   end
 
-  #Check if the data for insert in table don't exists in.
-  date_from_search_data = Date.today.prev_day
-  #check = check_if_data_exist(date_from_search_data, "date")
-
-  #if @table.exists? && check_if_data_exist(date_from_search_data, "date")
   if @table.exists?
-    inserter = @table.insert_async do |result|
-      if result.error?
-        raise "Error when table try to insert data in BBDD - #{result.error}"
-      else
-        puts "inserted #{result.insert_count} rows " \
-          "with #{result.error_count} errors"
-      end
-    end
-    
-    inserter.insert data
-    
-    inserter.stop.wait!
-  else
-    raise "Error table don't exist or the data to try insert in BBDD previously exists - #{result.error}"
-  end
+    response = @table.insert data
 
-  def search(data, from_field)
-    @bigquery.query("SELECT COUNT(*) as count FROM #{@table} WHERE #{from_field} = '#{data}'")
-  end
-
-  def deleteAll
-    "DELETE FROM #{@table} WHERE 1=1"
-  end
-
-  def check_if_data_exist(data)
-    result = search(data).first
-    if result["count"] > 0
-      puts "Data exist"
+    if response.success?
+      puts "Inserted rows successfully"
     else
-      puts "Data don't exist"
+      raise "Failed to insert #{response.error_rows.count} rows"
     end
+  else
+    raise "Error table doesn't exist or the data to try insert in BBDD previously exists"
   end
 end
 end
