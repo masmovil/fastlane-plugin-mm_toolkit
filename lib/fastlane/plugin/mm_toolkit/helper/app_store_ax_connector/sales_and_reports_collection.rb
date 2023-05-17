4e935a2e131ca2554dbd209fe4d9fdc6b77eb583
Raúl Pedraza <raul.pedraza@masmovil.com>
2023-05-17 11:38:04 +0200

    Resolve Adri comments

--- lib/fastlane/plugin/mm_toolkit/helper/app_store_ax_connector/sales_and_reports_collection.rb
+++ lib/fastlane/plugin/mm_toolkit/helper/app_store_ax_connector/sales_and_reports_collection.rb
@@ -5,7 +5,9 @@ require_relative "sales_and_reports"
 
 class SalesAndReportsCollection
   attr_accessor :items
-
+  #For more information about types you go https://developer.apple.com/help/app-store-connect/reference/product-type-identifiers/
+  FREE_OR_PAID_APP = "1F"
+  
   def initialize(file)
     csv_text = Zlib::GzipReader.new(StringIO.new(file)).read.force_encoding("UTF-8")
     csv_data = CSV.parse(csv_text, headers: true, col_sep: "\t")
@@ -23,7 +25,7 @@ class SalesAndReportsCollection
   def rows
     date_to_save = Date.today
     # Filter with 1F because 1F is a new downloads
-    items_grouped_by_brand = @items.filter { |item| item.product_type_identifier == "1F" }.group_by(&:sku)
+    items_grouped_by_brand = @items.filter { |item| item.product_type_identifier == FREE_OR_PAID_APP }.group_by(&:sku)
     items_grouped_by_brand.map do |brand, items|
       total_units = items.map { |item| item.units.to_i }.reduce(0, :+)
       { brand: brand, units: total_units, date: date_to_save }
